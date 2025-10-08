9585c792a1685420bf3bdbe7ebd76940c175659f
Ben <benchapuma77@gmail.com>
2025-10-08 09:06:04 +0200

    Revert "Implemented error.tsx file"
    This reverts commit 68de3df36f6ab38cd66f43767256d54538dcaacf.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,23 +24,14 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
- 
-  try {
-    await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
-  } catch (error) {
-    // We'll also log the error to the console for now
-    console.error(error);
-    return {
-      message: 'Database Error: Failed to Create Invoice.',
-    };
-  }
- 
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -54,17 +45,11 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  try {
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-      `;
-  } catch (error) {
-    // We'll also log the error to the console for now
-    console.error(error);
-    return { message: 'Database Error: Failed to Update Invoice.' };
-  }
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
